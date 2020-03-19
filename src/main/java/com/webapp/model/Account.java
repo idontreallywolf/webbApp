db2301409a42b630d55b60227f54fc959135d3ba
idontreallywolf <nedim.kanat1999@gmail.com>
2020-03-19 18:34:31 +0100

    add missing column to account model

--- src/main/java/com/webapp/model/Account.java
+++ src/main/java/com/webapp/model/Account.java
@@ -1,16 +1,15 @@
 package com.webapp.model;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
 public class Account {
     @Id
     @Column(name="id")        private int id;
     @Column(name="firstname") private String firstname;
     @Column(name="lastname")  private String lastname;
     @Column(name="username")  private String username;
+    @Column(name="password")  private String password;
     @Column(name="email")     private String email;
 
     public int    getId()        { return this.id; }
@@ -18,12 +17,14 @@ public class Account {
     public String getFirstname() { return this.firstname; }
     public String getLastname()  { return this.lastname; }
     public String getUsername()  { return this.username; }
+    public String getPassword()  { return this.password; }
     public String getEmail()     { return this.email; }
-    public String toString()     { return getId()+" | "+getFullName()+" | "+getUsername()+" | "+getEmail(); }
+    public String toString()     { return getId()+" | "+getFullName()+" | "+getUsername()+" | "+getPassword()+" | "+getEmail(); }
 
-    public void setId(int id) { this.id = id; }
+    public void setId(int id)              { this.id = id; }
     public void setFirstname(String fname) { this.firstname = fname; }
     public void setLastname(String lname)  { this.firstname = lname; }
     public void setUsername(String uname)  { this.username = uname;  }
+    public void setPassword(String pass)   { this.password = pass;   }
     public void setEmail(String email)     { this.email = email;     }
 }
