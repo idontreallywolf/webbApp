b9f7d710f27f2ca6499b5cbc5155c4ab8e65a629
idontreallywolf <nedim.kanat1999@gmail.com>
2020-03-19 18:37:37 +0100

    add validator methods for registerController

--- src/main/java/com/webapp/Config.java
+++ src/main/java/com/webapp/Config.java
@@ -16,7 +16,26 @@ import com.webapp.dao.AccountDaoImpl;
 public class Config {
 	public static String mainTitle = "PepeSite";
 	public static String rootDir = System.getProperty("user.dir");
-	
+
+    /**
+    *   Configuration for Accounts
+    */
+    public static class Account {
+        public static int minNameLength = 4;
+        public static int maxNameLength = 20;
+
+        public static int minUnameLength = 4;
+        public static int maxUnameLength = 30;
+
+        public static int minPassLength = 8;
+        public static int maxPassLength = 60;
+        
+        public static int minEmailLength = 6;
+    }
+
+    /**
+    *   Database Config
+    */
 	private static class Database {
 		private static String host = "127.0.0.1";
 		private static String user = "root";
@@ -28,6 +47,10 @@ public class Config {
 			return "jdbc:mysql://"+host+":"+port+"/"+dbname;
 		}
 	}
+
+    /**
+    *   BEANS
+    */
 
 	@Bean
 	public DriverManagerDataSource getDataSource() {
@@ -43,7 +66,7 @@ public class Config {
 	public AccountDao getAccountDao() {
 		return new AccountDaoImpl(getDataSource());
 	}
-	
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
--- src/main/java/com/webapp/controller/RegisterController.java
+++ src/main/java/com/webapp/controller/RegisterController.java
@@ -1,17 +1,163 @@
 package com.webapp.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.webapp.Config;
+import com.webapp.model.Account;
+import com.webapp.model.RegisterForm;
 
 @Controller
 public class RegisterController extends PageController implements PageControllerInterface {
 
-	@GetMapping("/register")
+    @GetMapping("/register")
     public ModelAndView run(){
         ModelAndView mv = new ModelAndView("index");
         mv = initDefaultAttributes("Register Account", "login.css", "register", mv);
         return mv;
     }
-    
+
+    @PostMapping("/register")
+    @ResponseBody
+    public Map<String, List<String>> registerAccount(@RequestBody RegisterForm regForm){
+        Map<String, List<String>> errors = new HashMap<String, List<String>>();
+
+        errors.put("firstname", validateName(regForm.getFirstname()));
+        errors.put("lastname",  validateName(regForm.getLastname()));
+        errors.put("username",  validateUsername(regForm.getUsername()));
+        errors.put("password",  validatePassword(regForm.getPassword(), regForm.getPasswordConfirm()));
+        errors.put("email",     validateEmail(regForm.getEmail()));
+
+        return errors;
+    }
+
+
+    /**
+    *	Validates names
+    * 	@param name
+    *
+    *	@return a List<String> of errors ( if any. Otherwise an empty list )
+    * */
+    public List<String> validateName(String name){
+        List<String> errors = new ArrayList<String>();
+
+        if(name.length() < Config.Account.minNameLength)
+            errors.add("err#minLen");
+        else if(name.length() > Config.Account.maxNameLength)
+            errors.add("err#maxLen");
+
+        // Check for non-alpha characters; symbols & numbers
+        Pattern p = Pattern.compile("[^a-zA-Z]+?");
+        Matcher m = p.matcher(name);
+
+        if (m.find())
+            errors.add("err#sym");
+
+        return errors;
+    }
+
+
+    /**
+    *   Validates the username
+    *
+    *   @param uname - username to be validated
+    *   @return a List<String> of errors ( if any. Otherwise an empty list )
+    */
+    public List<String> validateUsername(String uname){
+        List<String> errors = new ArrayList<String>();
+
+        if(uname.length() < Config.Account.minUnameLength)
+            errors.add("err#minLen");
+        else if(uname.length() > Config.Account.maxUnameLength)
+            errors.add("err#maxLen");
+
+        // Validate against non-alpha(&-numeric)
+        Pattern p = Pattern.compile("[^a-zA-Z0-9]");
+        Matcher m = p.matcher(uname);
+
+        if (m.find())
+            errors.add("err#sym");
+
+        // TODO: check if username is already in use
+
+        return errors;
+    }
+
+    /**
+    *   Validates password
+    *
+    *   @param password
+    *   @return  a List<String> of errors ( if any. Otherwise an empty list )
+    */
+    public List<String> validatePassword(String password, String passwordConfirm) {
+        List<String> errors = new ArrayList<String>();
+
+        if(!password.equals(passwordConfirm))
+            errors.add("err#cnfrm");
+
+        if(password.length() < Config.Account.minPassLength)
+            errors.add("err#minLen");
+        else if(password.length() > Config.Account.maxPassLength)
+            errors.add("err#maxLen");
+
+        // Check if password contains atleast 1 ..
+        // .. of each ( upperCase, lowerCase, number )
+        boolean lowerCase = false;
+        boolean upperCase = false;
+        boolean isDigit   = false;
+
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+
+            if(Character.isDigit(ch))
+                isDigit = true;
+            else if(Character.isUpperCase(ch))
+                upperCase = true;
+            else if(Character.isLowerCase(ch))
+                lowerCase = true;
+
+            if(isDigit && upperCase && lowerCase)
+                break;
+        }
+
+        if(!lowerCase || !upperCase || !isDigit)
+            errors.add("err#badSec");
+
+        return errors;
+    }
+
+    /**
+    *   Validates email
+    *
+    *   @param email
+    *   @return  a List<String> of errors ( if any. Otherwise an empty list )
+    */
+    public List<String> validateEmail(String email) {
+        List<String> errors = new ArrayList<String>();
+
+        if(email.length() < Config.Account.minEmailLength)
+        	errors.add("err#len");
+
+        String regexPattern = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
+                            + "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+
+        Pattern p = Pattern.compile(regexPattern, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(email);
+
+        if (!m.find())
+            errors.add("err#inv");
+
+        return errors;
+    }
 }
