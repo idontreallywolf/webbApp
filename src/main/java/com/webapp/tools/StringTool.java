ebc378df1ec4592a366d4d16ddc42aa37d995897
idontreallywolf <nedim.kanat1999@gmail.com>
2020-03-25 15:56:43 +0100

    make first letter of names in uppercase

---
+++ src/main/java/com/webapp/tools/StringTool.java
@@ -0,0 +1,18 @@
+package com.webapp.tools;
+
+public class StringTool {
+	private String string;
+	
+	public StringTool(String s) {
+		this.string = s;
+	}
+	
+	/**
+	 * Returns given string with the first letter in Uppercase
+	 * 
+	 * @return Capitalized string
+	 * */
+	public static String ucFirst(String s) {
+		return s.substring(0, 1).toUpperCase() + s.substring(1);
+	}
+}
