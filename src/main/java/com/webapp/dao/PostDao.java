7a5db7d628b583374c82f4bb58ed5093385526c5
idontreallywolf <nedim.kanat1999@gmail.com>
2020-03-27 20:16:42 +0100

    add missing method to postDao

--- src/main/java/com/webapp/dao/PostDao.java
+++ src/main/java/com/webapp/dao/PostDao.java
@@ -2,11 +2,13 @@ package com.webapp.dao;
 
 import java.util.List;
 
+import com.webapp.enums.Category;
 import com.webapp.model.Post;
 
 public interface PostDao {
     public List<Post> getPosts();
     public List<Post> getPostsByAuthorId(int authorId);
+    public List<Post> getPostsByCategory(Category categoryType);
     public Post getPostById(int postId);
-    public void    createPost(int authorId, String title,  String content);
+    public void createPost(int authorId, String title,  String content);
 }
