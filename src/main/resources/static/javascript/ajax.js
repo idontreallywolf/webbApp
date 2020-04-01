e5720b305508f5d86a9861fc0dac692f9b1ae119
idontreallywolf <nedim.kanat1999@gmail.com>
2020-04-01 18:47:42 +0200

    handle login/register form requests

--- src/main/resources/static/javascript/ajax.js
+++ src/main/resources/static/javascript/ajax.js
@@ -1,5 +1,42 @@
 $(document).ready(function(){
 
+    /**
+    *    Returns a dynamically created alert-box
+    *
+    *    @param {string} type - default|warning|danger|info|success
+    *    @param {string} body - content of alert box
+    *    @returns DOM Object
+    */
+    function newAlertBox(type, body) {
+        let alertBox   = $('<div class="alert-box alert-'+type+'"></div>');
+        let alertBody  = $('<span>'+body+'</span>');
+        let alertClose = $('<span><a class="alertBox-close"><i class="fa fa-times"></i></a></span>');
+
+        alertBox.append(alertBody);
+        alertBox.append(alertClose);
+
+        return alertBox;
+    }
+
+    $(document).on('click', '.alert-box .alertBox-close', function() {
+        let this_ = this;
+        $(this).parents('.alert-box').fadeOut(function(){
+            $(this_).parents('.alert-box').remove();
+        });
+    });
+
+    $('.inputHolder > input').focus(function(){
+        let sibling = $(this).siblings('.inputErrorBox');
+        if(sibling.children().length > 0){
+            sibling.show();
+        }
+    });
+
+    $('.inputHolder > input').focusout(function(){
+        let sibling = $(this).siblings('.inputErrorBox');
+        sibling.hide();
+    });
+
     $('.loginForm').submit(function(event){
         event.preventDefault();
         $.ajax({
@@ -11,7 +48,22 @@ $(document).ready(function(){
             type:'POST',
             data: getFormData($(this)),
             dataType: 'json',
-            success: function(data){ console.log(data); },
+            success: function(data){
+                console.log(data);
+                let response = data.response;
+                let el = $('.loginForm .responseBox');
+                if(response == "err#noacc" || response == "err#wuop")
+                    el.html(newAlertBox('danger', 'wrong username or password'));
+                else if (response == "err#active")
+                    el.html(newAlertBox('warning', 'A session is already active.'));
+                else if (response == "success") {
+                    el.html(newAlertBox('success', 'You will be redirected in a moment ...'));
+                    setTimeout(function(){
+                        window.location.href = "/";
+                    }, 2000);
+                }
+
+            },
             error: function(data){ console.log("ERR:" + JSON.stringify(data)); }
         });
     });
@@ -28,7 +80,68 @@ $(document).ready(function(){
             type: 'POST',
             data: getFormData($(this)),
             dataType: 'json',
-            success: function(data){ console.log(data); },
+            success: function(data){
+                console.log(data);
+                if(data.hasOwnProperty('error')){
+                    if(data.error == "err#sess")
+                        $('.responseBox').html(newAlertBox('danger', 'There\'s already an active session.'));
+
+                    if(data.error == "err#reg")
+                        $('.responseBox').html(newAlertBox('warning', 'Something went wrong. Contact web-admin!'));
+
+                    return;
+                }
+
+                if(data.hasOwnProperty('counter')){
+                    if(parseInt(data.counter) == 0){
+                        $('.responseBox').html(newAlertBox('success', 'Account created! You will be redirected in a moment ...'));
+
+                        setTimeout(function(){
+                            window.location.href = "/";
+                        }, 2000);
+
+                        return;
+                    }
+                }
+
+                // handle input errors
+                $.each(data, function(key, value){
+
+                    // skip when key is counter
+                    // since there are no input fields with that name
+                    if(key == 'counter')
+                        return true;
+
+                    let errors = value.replace(/((\[)|(\s))(err#)?|(\])/gi, "").split(',');
+
+                    let input = $('input#'+key);
+                    let inErrBox = input.siblings('.inputErrorBox');
+                    inErrBox.html("");
+
+                    if(errors[0].length > 0){
+                        if(input.hasClass('success'))
+                            input.removeClass('success');
+
+                        if(!input.hasClass('error'))
+                            input.addClass('error')
+
+                        let ul = $('<ul></ul>');
+                        for (var i = 0; i < errors.length; i++) {
+                            ul.append('<li>'+registerFormErrors[key][errors[i]]+'</li>');
+                        }
+                        inErrBox.append(ul);
+                        inErrBox.show();
+                    } else {
+                        if(input.hasClass('error'))
+                            input.removeClass('error');
+
+                        if(!input.hasClass('success'))
+                            input.addClass('success');
+
+                        inErrBox.hide();
+                    }
+                });
+            },
             error: function(data){ console.log("ERR:" + JSON.stringify(data)); }
         });
     });
